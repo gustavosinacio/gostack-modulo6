a6e21f195d1c8e45788a12352f22b6a15d81d4ef
Gustavo <gsinacio94@gmail.com>
2019-07-15 18:17:37 -0300

    Minor fixes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <h1>Hiya!</h1>
-  );
+  return <h1>Hiya!</h1>;
 }
 
 export default App;
